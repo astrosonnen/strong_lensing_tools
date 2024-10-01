7d8704b75a275e2c741928fcd407cff2e9abe8ca
Alessandro Sonnenfeld <alesonny@gmail.com>
2024-10-01 14:59:50 +0800

    new branch, new lens

---
+++ papers/new_lens/lenspars.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+zd = 1.055 # lens redshift
+zs = 2.792 # source redshift
+
+reff = 2.3 # half-light radius in kpc
+rein = 1.8 # Einstein radius in kpc
+rein_err = 0.3 # 1-sigma uncertainty on the Einstein radius
