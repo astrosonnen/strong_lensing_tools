d604744e94ea35ac210a15e272b21fcbbfc2e25a
Alessandro Sonnenfeld <alesonny@gmail.com>
2024-10-03 11:37:30 +0800

    defining theta_Ein first

--- papers/new_lens/lenspars.py
+++ papers/new_lens/lenspars.py
@@ -9,14 +9,13 @@ zs = 2.792 # source redshift
 s_cr = Sigma_cr(zd, zs)
 arcsec2kpc = arcsec2kpc_func(zd)
 
-reff_kpc = 2.3 # half-light radius in kpc
-rein_kpc = 1.8 # Einstein radius in kpc
-rein_err = 0.3 # 1-sigma uncertainty on the Einstein radius
+tein_obs = 0.21 # Einstein radius in arcsec
+tein_err = 0.04 # Uncertainty on the Einstein radius
 
-# Einstein radius in arcsec.
-# Computing from rein_kpc, but the other way around would be better
-tein_obs = rein_kpc / arcsec2kpc
-tein_err = rein_err / arcsec2kpc
+reff_kpc = 2.3 # half-light radius in kpc
+
+rein_kpc = tein_obs * arcsec2kpc # Einstein radius in kpc
+rein_err = tein_err * arcsec2kpc # 1-sigma uncertainty on the Einstein radius
 
 lmsps_obs = np.log10(3.1e10) # log observed stellar mass
 lmsps_err = 0.5*(np.log10(3.7e10) - np.log10(2.7e10)) # uncertainty on log(Mstar)
